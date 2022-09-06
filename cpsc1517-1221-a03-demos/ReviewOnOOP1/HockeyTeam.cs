204a4b1842748327bfa9bd5a2d7ddc85750d82b8
PavneetRai <97554036+PavneetRai@users.noreply.github.com>
2022-09-06 14:50:48 -0600

    fs
    fs

--- cpsc1517-1221-a03-demos/ReviewOnOOP1/HockeyTeam.cs
+++ cpsc1517-1221-a03-demos/ReviewOnOOP1/HockeyTeam.cs
@@ -8,5 +8,23 @@ namespace ReviewOnOOP1
 {
     public class HockeyTeam
     {
+        public enum HockeyConference { Eastern, Western}
+
+        public enum HockeyDivision { Metropolitan, Alantic, Centeral, Pacific}
+
+        //defing data feilds for storing data 
+        private HockeyConference _conference; 
+        private HockeyDivision _division;
+
+        //define fully-implmented properites for the data feilds 
+        public HockeyConference Conferecce
+        {
+            get { return _conference; }
+        }
+        public HockeyDivision Division
+        {
+            get { return _division; }
+            set { _division = value; }
+        }
     }
 }
