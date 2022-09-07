169907a837e515c073287be1025994757161a8dd
PavneetRai <97554036+PavneetRai@users.noreply.github.com>
2022-09-07 16:53:38 -0600

    Object review

---
+++ cpsc1517-1221-a03-demos/ReviewOnOOP1/NhlTeam.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewOnOOP1
+{
+    public class NhlTeam
+    {
+
+        public NhlConference Conference { get; private set; }
+        public NhlDivision Division { get; private set; }
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("name must contain text");
+
+                }
+                _name = value.Trim();
+            }
+        }
+        private string _city;
+        public string City
+        {
+            get => _city;
+            private set {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("city must contain text");
+
+                }
+                _city = value.Trim();
+            }
+        }
+        private int _gamesplayed;
+        public int Gamesplayed
+        {
+            get=> _gamesplayed;
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("must be more than one ");
+
+                }
+                _gamesplayed = value;
+            }
+        }
+        private int _wins;
+        public int Wins 
+        { 
+            get => _wins; 
+            set 
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("must be more than one");
+                }
+                _wins = value;
+            }
+                     
+        }
+        private int _losses;
+        public int Lossess
+        {
+            get => _losses;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("must be more than one ");
+                }
+                _losses = value;
+            }
+
+        }
+        private int _overtimelosses;
+        public int Overtimelosses
+        {
+            get => _overtimelosses;
+            set 
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("must be more than one ");
+                }
+                _overtimelosses = value;
+            } 
+
+        }
+        
+        public int Points
+        {
+            get => (Wins * 2) + Overtimelosses;
+
+        }
+         public NhlTeam(NhlConference conference, NhlDivision division, string name, string city)
+        {
+            Conference = conference;
+            Division = division;    
+            Name = name;
+            this.City = city;
+
+            Gamesplayed = 0;
+            Wins = 0;
+            Lossess = 0;
+            Overtimelosses =0;
+
+
+        }
+        public override string ToString()
+        {
+           // return base.ToString();
+           return $"{Conference},{Division},{Name}, {City}, {Gamesplayed},{Wins},{Lossess},{Overtimelosses}";
+        }
+
+        ////defing data feilds for storing data 
+        //private HockeyConference _conference; 
+        //private HockeyDivision _division;
+
+        ////define fully-implmented properites for the data feilds 
+        //public HockeyConference Conferecce
+        //{
+        //    get { return _conference; }
+        //}
+        //public HockeyDivision Division
+        //{
+        //    get { return _division; }
+        //    set { _division = value; }
+        //}
+    }
+}
--- cpsc1517-1221-a03-demos/ReviewOnOOP1/Program.cs
+++ cpsc1517-1221-a03-demos/ReviewOnOOP1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ReviewOnOOP1;
+
 Console.WriteLine("Helloo, World!");
+
+var senators = new NhlTeam(NhlConference.Eastern, NhlDivision.Alantic, "senators", "ottawa");
+senators.Gamesplayed = 82;
+senators.Wins = 33;
+senators.Lossess = 42;
+senators.Overtimelosses = 7;
+//print the points 
+
+Console.WriteLine(senators);
+Console.WriteLine($"Points = {senators.Points}");
