d593e706ccd4bcba576f0382de4d2291241e1903
prai6 <prai6@WB304-13>
2022-10-19 16:24:01 -0600

    Client Server ArchitectureFile

---
+++ WestwindSolution/WestwindSystem/Entity/EntityNotes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WestwindSystem.Entity
+{
+    internal class EntityNotes
+    {
+        //an entity is class that is mapped to the database table 
+    }
+}
