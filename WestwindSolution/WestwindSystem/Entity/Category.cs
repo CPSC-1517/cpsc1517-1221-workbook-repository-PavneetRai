607b3ee475d2af683cc4c74396c958c175c3a541
prai6 <prai6@WA320-12>
2022-10-25 14:43:54 -0600

    RazorPages Crud 10/25/2022

--- WestwindSolution/WestwindSystem/Entity/Category.cs
+++ WestwindSolution/WestwindSystem/Entity/Category.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WestwindSystem.Entity
 {
+    [Table(name:"Categories")]
     public class Category
     {
+        [Key]
+        [Column(name:"CategoryID")]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="CatergoryName is required")]
+        [MaxLength(15, ErrorMessage ="CategoryName max length is 15")]
+        public string CategoryName { get; set; }
+        [Column(TypeName="ntext")]
+        public string Description { get; set; }
+        [Column(TypeName ="varbinary")]
+        public byte[] Picture { get; set; }
+
+        public string PictureMimeType { get; set; }
+        
+
     }
 }
